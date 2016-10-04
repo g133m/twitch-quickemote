b280a20ca56e6677bd822c3e86033dc81727cbc7
Clint Powell <clint@cpsoft.io>
2016-10-04 16:39:59 -0400

    Update script version, add favicon and resources

--- wizard/js/quickEmote.user.js
+++ wizard/js/quickEmote.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Twitch Quick Emote Buttons
-// @version      0.1.3
+// @version      0.1.4
 // @match        https://www.twitch.tv/*
 // @match        https://g133m.github.io/twitch-quickemote/wizard/
 // @copyright    2016+, g133m
 // @author       g133m
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
-// @require      https://g133m.github.io/twitch-quickemote/wizard/js/main.js
+// @require      https://g133m.github.io/twitch-quickemote/wizard/js/main-0.1.4.js
 // @namespace    https://g133m.github.io/twitch-quickemote
 // @description  Add buttons to quickly send emote-heavy messages to chat
 // @grant        unsafeWindow
@@ -15,17 +15,17 @@
 // @run-at       document-idle
 // ==/UserScript==
 (function() {
-	'use strict';
+    'use strict';
     var btnSaved = GM_getValue('g133mbuttons', '');
     var buttons = [];
 
-	if(btnSaved) {
-		buttons = JSON.parse(btnSaved);
+    if(btnSaved) {
+        buttons = JSON.parse(btnSaved);
     } else {
-		GM_setValue("g133mbuttons", '[]');
-		localStorage.g133mbuttons_data = '[]';
-	}
-    unsafeWindow.__g133mbuttonsVERSION = '0.1.3';
+        GM_setValue("g133mbuttons", '[]');
+        localStorage.g133mbuttons_data = '[]';
+    }
+    unsafeWindow.__g133mbuttonsVERSION = '0.1.4';
  unsafeWindow.__g133mbuttonsSAVED = false;
 if(localStorage.g133mbuttons_savestatus == 'saving') {
         buttons = JSON.parse(localStorage.g133mbuttons_data);
@@ -36,6 +36,6 @@ if(localStorage.g133mbuttons_savestatus == 'saving') {
 
 
 initializeEmoteButtons(
-	buttons
+    buttons
 );
 })();
