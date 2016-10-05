9d866eeffe403c832d68aa922e5acfc4e511aa4d
Clint Powell <clint@cpsoft.io>
2016-10-05 14:09:01 -0400

    Fix bug where emotes are added to whispers chat input

--- wizard/js/quickEmote.user.js
+++ wizard/js/quickEmote.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Twitch Quick Emote Buttons
-// @version      0.1.4
+// @version      0.1.5
 // @match        https://www.twitch.tv/*
 // @match        https://g133m.github.io/twitch-quickemote/wizard/
 // @copyright    2016+, g133m
 // @author       g133m
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
-// @require      https://g133m.github.io/twitch-quickemote/wizard/js/main-0.1.4.js
+// @require      https://g133m.github.io/twitch-quickemote/wizard/js/main-0.1.5.js
 // @namespace    https://g133m.github.io/twitch-quickemote
 // @description  Add buttons to quickly send emote-heavy messages to chat
 // @grant        unsafeWindow
@@ -25,7 +25,7 @@
         GM_setValue("g133mbuttons", '[]');
         localStorage.g133mbuttons_data = '[]';
     }
-    unsafeWindow.__g133mbuttonsVERSION = '0.1.4';
+    unsafeWindow.__g133mbuttonsVERSION = '0.1.5';
  unsafeWindow.__g133mbuttonsSAVED = false;
 if(localStorage.g133mbuttons_savestatus == 'saving') {
         buttons = JSON.parse(localStorage.g133mbuttons_data);
