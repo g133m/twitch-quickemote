6d8128373b54e12537417d562995d2a3eca98f85
g133m <g133mstream@gmail.com>
2018-06-04 12:17:51 -0400

    Create quickEmote.meta.js

---
+++ wizard/js/quickEmote.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Twitch Quick Emote Buttons
+// @version      0.1.6
+// @match        https://www.twitch.tv/*
+// @match        https://g133m.github.io/twitch-quickemote/wizard/
+// @copyright    2018+, g133m
+// @author       g133m
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @downloadURL  https://g133m.github.io/twitch-quickemote/wizard/js/quickEmote.user.js
+// @updateURL    https://g133m.github.io/twitch-quickemote/wizard/js/quickEmote.meta.js
+// @namespace    https://g133m.github.io/twitch-quickemote
+// @description  Add buttons to quickly send emote-heavy messages to chat
+// @grant        unsafeWindow
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @run-at       document-idle
+// ==/UserScript==
